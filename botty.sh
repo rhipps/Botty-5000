caa7af47c53f0835334baf77599a920d19b24b6f
Ryan Hipps <ryanhippsdesign@gmail.com>
2020-09-21 23:15:26 -0400

    Bash Shell script to help with running Botty

---
+++ botty.sh
@@ -0,0 +1,42 @@
+function find_botty {
+  echo "Finding Botty process..."
+  BOTTYPID=$(pgrep -f "python3 brains.py")
+  export BOTTYPID
+}
+
+function stop_botty {
+  find_botty
+  if [ ! -z "$BOTTYPID" ]; then
+    echo "Found Botty process @PID: $BOTTYPID"
+    echo "Killing botty process..."
+    kill ${BOTTYPID}
+  else
+        echo "No Botty process found"
+  fi
+}
+
+function start_botty {
+  echo "Starting Botty Process..."
+  python3 brains.py &
+  find_botty
+  if [ ! -z "$BOTTYPID" ]; then
+    echo "Found Botty process @PID: $BOTTYPID"
+  else
+    echo "No Botty process found"
+  fi
+}
+
+if [ ! -z "$1" ]; then
+  ARGUMENT=$1
+  echo "Going to $ARGUMENT Botty"
+  if [[ "$ARGUMENT" == "stop" || "$ARGUMENT" == "bounce" ]]; then
+    stop_botty
+  fi
+
+  if [[ "$ARGUMENT" == "start" || "$ARGUMENT" == "bounce" ]]; then
+    start_botty
+  fi
+
+else
+  echo "Missing a script argument [start, stop, bounce]"
+fi
